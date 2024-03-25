495ae743b938cb322596e61d49417f3648b2807c
saifffff <angryn1nj4@gmail.com>
2024-03-25 12:47:09 +0530

    learnt implementing custom finder methods, JPQL, and @param binding

--- src/main/java/com/firstboot/demo/SpringJpaApplication.java
+++ src/main/java/com/firstboot/demo/SpringJpaApplication.java
@@ -1,5 +1,6 @@
 package com.firstboot.demo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.boot.SpringApplication;
@@ -18,10 +19,45 @@ public class SpringJpaApplication {
 		BookRepository bs = ctx.getBean(BookRepository.class);
 		
 //		Book hp = new Book();
-//		hp.setName("Harry Potter");
+//		hp.setName("The Casual Vacancy");
 //		hp.setWriter("j.k.rowling");
-//		bs.save(hp);
-//		System.out.println("saved harry potter");
+//		
+//		Book hp1 = new Book();
+//		hp1.setName("Fantastic Beasts & Where to Find Them");
+//		hp1.setWriter("j.k.rowling");
+//		
+//		Book hp2 = new Book();
+//		hp2.setName("Quidditch Through the Ages");
+//		hp2.setWriter("j.k.rowling");
+//		
+//		Book hp3 = new Book();
+//		hp3.setName("The Silkworm");
+//		hp3.setWriter("j.k.rowling");
+//		
+//		Book mb1 = new Book();
+//		mb1.setName("To Con a Gentleman");
+//		mb1.setWriter("Sarah Adams");
+//		
+//		Book mb2 = new Book();
+//		mb2.setName("Practice Makes Perfect");
+//		mb2.setWriter("Sarah Adams");
+//		
+//		Book mb3 = new Book();
+//		mb3.setName("The Cheat Sheet");
+//		mb3.setWriter("Sarah Adams");
+//		
+//		Book mb4 = new Book();
+//		mb4.setName("When in Rome");
+//		mb4.setWriter("Sarah Adams");
+//		
+//		List<Book> myreads = new ArrayList<>();
+//		myreads.add(hp); myreads.add(hp1); myreads.add(hp2); myreads.add(hp3);
+//		myreads.add(mb1); myreads.add(mb2); myreads.add(mb3); myreads.add(mb4);
+//		
+//		bs.saveAll(myreads);
+//		System.out.println("save all books");
+		
+		
 		
 //		System.out.println("book deleted");
 //		bs.deleteById(102);
@@ -31,6 +67,11 @@ public class SpringJpaApplication {
 //			System.out.println(book);
 //		}
 		
+		
+		List<Book> allbyWriter = bs.findByWriter("Sarah Adams");
+		for(Book bk : allbyWriter) {
+			System.out.println(bk);
+		}
 	}
 
 }
--- src/main/java/com/firstboot/demo/dao/BookRepository.java
+++ src/main/java/com/firstboot/demo/dao/BookRepository.java
@@ -1,8 +1,11 @@
 package com.firstboot.demo.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.firstboot.demo.entity.Book;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
+	public List<Book> findByWriter(String writer);
 }
